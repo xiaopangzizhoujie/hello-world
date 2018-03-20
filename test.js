c2f116d68726c0a04f532c7a7cd5a596af7dee71
xiaopangzizhoujie <37565723+xiaopangzizhoujie@users.noreply.github.com>
2018-03-19 20:23:39 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
 <script type="text/javascript">  
-        const json = {
-            string: 'hello world',
-            number: 100,
-            date: new Date(),
-            buf: new Buffer('hello world')
-        };
+    const json = {
+        string: 'hello world',
+        number: 100,
+        date: new Date(),
+        buf: new Buffer('hello world')
+    };
 
     function encode(json){
-        var jsonText = JSON.stringify(json);
+        var jsonText=JSON.stringify(json);
         return jsonText;
     }
 
     function decode(jsonStr){
-        var jsonobject = JSON.parse(jsonText,function(key,value){  
-            if(key == "date"){  
+        var jsonobject=JSON.parse(jsonText,function(key,value){  
+            if(key=="date"){  
                 return new Date(value);  
-            }else{  
+            }else if(key=="buf"){  
                 return new Buffer(value);
             }else{
                 return value;  
